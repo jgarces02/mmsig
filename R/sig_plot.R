179eb500fcb977cdaf33a9501e6025143227ac51
Juan-Jose Garces <27858969+jgarces02@users.noreply.github.com>
2025-01-16 16:35:08 -0500

    Update sig_plot.R

--- R/sig_plot.R
+++ R/sig_plot.R
@@ -1,8 +1,9 @@
 #' Plotting function for mmsig package
 #'
 #' @param sig Object generated through \code{mm_fit_signatures}
-#' @param sig_order order of signatures to plot in stacked bar chart, from top to bottom
-#' @param samples boolean whether or not to plot sample names on the x axis
+#' @param plot.type Type of barplot. Options are 'stacked' (default) or '96'.
+#' @param sig_order Order of signatures in \code{stacked} barplot, from top to bottom.
+#' @param plot.names boolean whether or not to plot sample names on the x axis. Default = \code{FALSE}
 #' @param col.width Widht for col bars. Default = 0.9
 #'
 #' @return relative contribution of mutational signatures in each sample
@@ -21,32 +22,12 @@
 #' @importFrom ggplot2 labs
 #' @export
 #'
-plot_signatures = function(sig,
+plot_signatures = function(sig, plot.type = "stacked",
                            sig_order = c("SBS1", "SBS2", "SBS13", "SBS5", "SBS8", "SBS9", "SBS18", "SBS-MM1", "SBS35"),
-                           samples = FALSE, col.width = 0.9){
+                           plot.names = FALSE, col.width = 0.9){
 
-  rotatedAxisElementText = function(angle,position='x'){
-    angle     = angle[1];
-    position  = position[1]
-    positions = list(x=0,y=90,top=180,right=270)
-    if(!position %in% names(positions))
-      stop(sprintf("'position' must be one of [%s]",paste(names(positions),collapse=", ")),call.=FALSE)
-    if(!is.numeric(angle))
-      stop("'angle' must be numeric",call.=FALSE)
-    rads  = (angle - positions[[ position ]])*pi/180
-    hjust = 0.5*(1 - sin(rads))
-    vjust = 0.5*(1 + cos(rads))
-    element_text(angle=angle,vjust=vjust,hjust=hjust)
-  }
-
-  scale_fill_sigs <- function(...){
-    ggplot2:::manual_scale('fill',
-                           values = setNames(c(RColorBrewer::brewer.pal(8, "Dark2"), "lightblue", "#FB8072"),
-                                             c("SBS1", "SBS2", "SBS5", "SBS8", "SBS9", "SBS13", "SBS18", "SBS-MM1", "SBS35", "SBS84")),
-                           ...)
-  }
-
-  sigPlot <- sig$estimate %>%
+  if(plot.type == "stacked"){
+    sigPlot <- sig$estimate %>%
     rownames_to_column(var = "sample") %>%
     dplyr::select(-mutations) %>%
     melt(id.var = "sample", variable.name = "SBS", value.name = "prop") %>%
@@ -66,12 +47,23 @@ plot_signatures = function(sig,
             legend.title = element_text(size = 14),
             axis.title.x = element_blank())
 
-  if(samples){
-    sigPlot +
-      theme(axis.text.x = rotatedAxisElementText(90, "top"))
-  } else{
-    sigPlot +
-      theme(axis.text.x = element_blank(),
-            axis.ticks.x = element_blank())
-  }
+      if(plot.names){
+        sigPlot +
+          theme(axis.text.x = rotatedAxisElementText(90, "top"))
+      } else{
+        sigPlot +
+          theme(axis.text.x = element_blank(),
+                axis.ticks.x = element_blank())
+      }
+    }else if(plot.type == "96"){
+      sig$mutmatrix %>% 
+      mutate(mut = rownames(.), mutgroup = gsub(".*.\\[|\\].*.", "", rownames(.))) %>% 
+      reshape2::melt() %>% 
+      ggplot(aes(x = mut, y = value, fill = mutgroup)) + geom_col() +
+        facet_grid(variable ~ mutgroup, scales = "free_x") + theme_bw() + 
+        scale_fill_manual(values = c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2")) +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1), panel.grid = element_blank(), legend.position = "none")
+    }else{
+      stop("ERROR: Please, indicate a correct plot type (ie, 'stacked' or '96' (as character, no number))")
+    }
 }
